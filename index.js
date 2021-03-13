db5f45b31f9fc3ba8948f199b7ab81ae78be0688
ishu9bansal <ishubansal1400@gmail.com>
2021-03-14 01:30:35 +0530

    add shuffle and bubble sort

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const offset = 10;
-const order = 10;
-const period = 1000;
-const quick = 200;
-const nodeRadius = 1;
-const nodeColor = 'aqua';
+const order = 8;
+const nodeColor = 'black';
 const lineColor = 'pink';
 const layers = [
-	'chart'
+	'line',
+	'node'
 ];
 
+var nodeRadius;
 var data;
 var limit;
 var svg;
@@ -17,7 +16,65 @@ var height;
 var layer;
 var chartScaleY;
 var chartScaleX;
+var colorScale;
 var index;
+var nodes;
+var lines;
+
+function render(){
+	nodes
+	.attr('cx', (d,i) => chartScaleX(i))
+	.attr('cy', (d,i) => chartScaleY(data[i]));
+
+	lines
+	.attr('x2', chartScaleX(index))
+	.attr('x1', chartScaleX(index))
+	.attr('y2', chartScaleY(data[index]||0))
+	.attr('y1', 0);
+}
+
+// function lineHandler(lineName, index, color){
+// 	if(color==null)	color = Math.random();
+// 	var i = lines.findIndex(d => d.name==lineName);
+// 	if(index<0||index>=limit&&i!=-1){
+// 		lines[i] = lines[lines.length-1];
+// 		lines.pop();
+// 	}
+// 	if(i==-1)	lines.push({
+// 		name: lineName,
+// 		color: color
+// 	});
+//	else	lines[i].index = index;
+// }
+
+async function shuffle(){
+	for(var i=data.length-1; i>=0; i--){
+		index = Math.floor(limit*Math.random());
+		render();
+		await new Promise(r => setTimeout(r, 0));
+		var t = data[i];
+		data[i] = data[index];
+		data[index] = t;
+	}
+	index = -1;
+	render();
+}
+
+async function sort(){
+	for(var l=data.length-1; l>=0; l--){
+		for(var i=0; i<l; i++){
+			index = i;
+			render();
+			await new Promise(r => setTimeout(r, 0));
+			if(data[i]<=data[i+1])	continue;
+			var t = data[i];
+			data[i] = data[i+1];
+			data[i+1] = t;
+		}
+	}
+	index = -1;
+	render();
+}
 
 function init(){
 	// calculate limit based on the order const
@@ -32,6 +89,8 @@ function init(){
 	width = window.innerWidth - 2*offset;
 	height = window.innerHeight - 2*offset;
 
+	nodeRadius = Math.max(2,width/limit)/2;
+
 	svg = d3.select("svg")
 	.attr("width", width).attr("height", height)
 	.attr("x", offset).attr("y", offset);
@@ -45,18 +104,23 @@ function init(){
 		layer[i] = svg.append('g');
 	}
 
-	layer['chart'].selectAll('circle.node')
+	index = Math.floor(limit*Math.random());
+	index = -1;
+	layer['line'].append('line').classed('index', true)
+	.attr('x1', chartScaleX(index)).attr('x2', chartScaleX(index))
+	.attr('y1', 0).attr('y2', chartScaleY(data[index]||0))
+	.style('stroke-width', nodeRadius)
+	.style('stroke', lineColor);
+
+	layer['node'].selectAll('circle.node')
 	.data(data).enter().append('circle')
 	.classed('node', true).attr('r', nodeRadius)
 	.attr('cx', (d,i) => chartScaleX(i))
-	.attr('cy', (d,i) => chartScaleY(d))
+	.attr('cy', (d,i) => chartScaleY(data[i]))
 	.style('fill', nodeColor);
 
-	index = Math.floor(limit*Math.random());
-	layer['chart'].append('line').classed('index', true)
-	.attr('x1', chartScaleX(index)).attr('x2', chartScaleX(index))
-	.attr('y1', 0).attr('y2', chartScaleY(data[index]))
-	.style('stroke', lineColor);
+	nodes = layer['node'].selectAll('circle.node');
+	lines = layer['line'].select('line.index');
 }
 
 init();
